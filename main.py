c730f08e872e7bddcfe69d79d1ce7639fd87bbd4
Duc Pham Le <ducphamle212@gmail.com>
2020-05-12 21:01:37 +0700

    update backward and forward

--- main.py
+++ main.py
@@ -27,16 +27,27 @@ def moveRight():
     return response('success', "moved right", 200)
 
 
+@app.route("/moveActions/forward", methods=["POST"])
+def moveForward():
+    request_json = request.get_json()
+    print("request json: ", request_json)
+    return response('success', "moved Forward", 200)
+
+
+@app.route("/moveActions/backward", methods=["POST"])
+def moveBackward():
+    request_json = request.get_json()
+    print("request json: ", request_json)
+    return response('success', "moved Backward", 200)
+
+
 @socketio.on('message')
 def handle_message(msg):
     print('received message: ' + msg)
     #send(msg, broadcast=True)
 
 
-# NOTE: THIS MAIN FILENAME MUST MATCH THE NAME IN THE PROCFILE
-# Eg: this file is named main.py, then in procfile => main:app
-
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, host='0.0.0.0', port=8088)
     # app.run(debug=True)
     #app.run(host='0.0.0.0', port='8088')
